85769188e72db5f9a0592de2e17d531635d86456
Gustavo Fontana Vieira <gustavoacvvieira@Outlook.com>
2022-02-09 22:07:01 -0300

    Início do curso de Javascript do iniciante ao avançado

---
+++ Udemy - The Complete JavaScript Course 2022/JavaScript Fundamentals - Part 1/starter/script.js
@@ -0,0 +1,29 @@
+let js = 'amazing';
+console.log(40+8+23-10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+//evita iniciar variáveis com a primeira letra em CAPS LOCK
+let person = 'jonas'
+
+//valores que nunca se alteram é utilizado como constante, evitando assim sobreescrever o valor da variável
+const PI = 3.1215;
+
+
+const country = "Brazil";
+let continent = "South America";
+let population = 212.6;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let language = "Portugues";
+
